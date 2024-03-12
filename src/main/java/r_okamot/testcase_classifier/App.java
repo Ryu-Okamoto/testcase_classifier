6597c69a83dbf078c186490612d729fb02af2ac5
r-okamot <r-okamot@ist.osaka-u.ac.jp>
2024-03-12 20:54:09 +0900

    refactor

---
+++ src/main/java/r_okamot/testcase_classifier/App.java
@@ -0,0 +1,8 @@
+package r_okamot.testcase_classifier;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/r_okamot/testcase_classifier/AppTest.java
@@ -0,0 +1,12 @@
+package r_okamot.testcase_classifier;
+
+import org.junit.jupiter.api.*;
+
+public class AppTest {
+    
+    @Test
+    public void test() {
+        
+    }
+    
+}
